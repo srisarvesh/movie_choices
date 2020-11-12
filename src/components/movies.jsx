573051f3063ceaef7ed805f64642b4bd8da98e90
Sarvesh <r.srisarvesh@gmail.com>
2020-11-12 23:31:25 +0530

    After adding adding delete button in the table and making it delete the movie object

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,7 +5,13 @@ class Movies extends Component{
 
     state={
         movies:getMovies()
-    }
+    };
+    handledelete=movie=>{
+        const movies=this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({movies});
+
+    };
+    
     render()
     {
         return(
@@ -15,17 +21,22 @@ class Movies extends Component{
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </thead>
                 <tbody>
-                    {this.state.movies.map(movies=>(
-                        <tr>
-                            <td>{movies.title}</td>
-                            <td>{movies.genre.name}</td>
-                            <td>{movies.numberInStock}</td>
-                            <td>{movies.dailyRentalRate}</td>
+                    {this.state.movies.map(movie=>(
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <button onClick={()=>this.handledelete(movie)} className="btn btn-danger btn-sm">Delete</button>
+                            </td>
                         </tr>
-                    ))}
+                          ))}
                 </tbody>
+                
             </table>
         )
     }
