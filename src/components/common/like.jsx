78a4f8edf9f3ee307215ffbdabb3e26769183010
Sarvesh <r.srisarvesh@gmail.com>
2020-11-16 18:12:58 +0530

    After adding like button to each row and the like button is an componenet which is applied to each and every row

---
+++ src/components/common/like.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export class Like extends Component{
+    render() {
+        return (
+             <i class="fa fa-heart-o" aria-hidden="true"></i>
+        );
+    }
+}
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React,{ Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-
+import {Like} from './common/like'
 class Movies extends Component{
 
     state={
@@ -31,6 +31,7 @@ class Movies extends Component{
                     <th>Stock</th>
                     <th>Rate</th>
                     <th></th>
+                    <th></th>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie=>(
@@ -39,6 +40,9 @@ class Movies extends Component{
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like />
+                            </td>
                             <td>
                                 <button onClick={()=>this.handledelete(movie)} className="btn btn-danger btn-sm">Delete</button>
                             </td>
