b2b9aadf15a5ea726783cae9041733dac2939948
Sarvesh <r.srisarvesh@gmail.com>
2020-11-17 11:56:19 +0530

    adding the page numbers dynammically by using the formula=Math.ceil(itemsCount/pageSize) and using the lodash we are creating an array and using map we render the pages dynamically and if the pagescount is 1 we are returning null and nothing is printed in the pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import _ from 'lodash';
 
 const Pagination = (props) => {
 
     const{ itemsCount,pageSize }=props;
+    const pagesCount=Math.ceil(itemsCount/pageSize);
+    const pages=_.range(1,pagesCount+1);
+    if(pagesCount===1){
+        return null;
+    }
+    
     return (
         <ul className="pagination">
-            
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-             
-             
+              {pages.map(page=>
+              
+              <li key={page }className="page-item"><a class="page-link" href="#">{page}</a></li>
+              )}
         </ul>
     );
 }
