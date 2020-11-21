f0de86ca0f49451eb4243957b929c200e6aaad17
Sarvesh <r.srisarvesh@gmail.com>
2020-11-21 17:58:32 +0530

    After linking to the movieform

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/not-Found';
 import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
 class App extends Component{
   state={}
   render()
@@ -18,14 +19,15 @@ class App extends Component{
       <NavBar/>
       <main className="Container">
         <Switch>
-        <Route path="/movies" exact component={Movies}></Route>
+        <Route path="/movies/:id" component={MovieForm}/>
+        <Route path="/movies"  component={Movies}></Route>
         <Route path="/customers" component={Customers}></Route>
         <Route path="/rentals" component={Rentals}></Route>
         <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" ></Redirect>
+        <Redirect from="/" exact to="/movies" />   
         <Redirect to="/not-found" />
         </Switch>
-      </main>
+      </main>  
       </div>
     );
   
