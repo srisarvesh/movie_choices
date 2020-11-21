1f5acc59fa58b6da57054dd8883441ca2f8da2ef
Sarvesh <r.srisarvesh@gmail.com>
2020-11-21 16:36:02 +0530

    After adding Routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import './App.css';
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-
+import {Redirect, Route, Switch} from 'react-router-dom';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/not-Found';
 class App extends Component{
   state={}
   render()
   {
     return(
       <main className="Container">
-        <Movies/>
+        <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" component={NotFound}></Redirect>
+        <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   
   }
   
-}
+} 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Movies />
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
