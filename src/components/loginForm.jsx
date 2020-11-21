baccaeb9658f2b26b673a4de0c1f4c28650b2b01
Sarvesh <r.srisarvesh@gmail.com>
2020-11-21 23:29:08 +0530

    After performing some clean code by destructurring some objects

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -12,26 +12,27 @@ class LoginForm extends Component {
             e.preventDefault();
            
         };
-        handleChange=e=>{
+        handleChange=({currentTarget:input})=>{
             const account={...this.state.account};
-            account[e.currentTarget.name ]=e.currentTarget.value;
+            account[input.name ]=input.value;
             this.setState({account});
 
         }
     
     render() { 
-      
+      const{account}=this.state;
         return (
+            
             <div className="container">
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input value={this.state.account.username} name="username" onChange={this.handleChange} autoFocus ref={this.username} id="username" type="text" className="form-control"/>
+                    <input value={account.username} name="username" onChange={this.handleChange} autoFocus ref={this.username} id="username" type="text" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input value={this.state.account.password} name="password" onChange={this.handleChange}  id="password" type="text" className="form-control"/>
+                    <input value={account.password} name="password" onChange={this.handleChange}  id="password" type="text" className="form-control"/>
                 </div>
                 <button className="btn btn-primary">Login</button>
             </form>
