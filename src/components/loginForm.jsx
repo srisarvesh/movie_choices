c75480cef580e07647b72912691eacba59a30cf2
Sarvesh <r.srisarvesh@gmail.com>
2020-11-21 22:16:19 +0530

    After handling the submit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import { Button } from 'bootstrap';
 
 class LoginForm extends Component {
+      handleSubmit=e=>{
+            e.preventDefault();
+            console.log("Submmited")
+        };
     
     render() { 
+      
         return (
             <div className="container">
             <h1>Login</h1>
-            <form >
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
                     <input id="username" type="text" className="form-control"/>
