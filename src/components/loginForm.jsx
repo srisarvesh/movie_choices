69208e48e179b894ca20ddd9b9d0e9cba0222428
Sarvesh <r.srisarvesh@gmail.com>
2020-11-21 22:38:10 +0530

    creating referene by giving username=React.createRef() and in the input field of username we give autoFocus

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 import { Button } from 'bootstrap';
 
 class LoginForm extends Component {
+    username=React.createRef();
       handleSubmit=e=>{
             e.preventDefault();
             console.log("Submmited")
+            const username=this.username.current.value;
+            console.log(username);
         };
     
     render() { 
@@ -16,7 +19,7 @@ class LoginForm extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" className="form-control"/>
+                    <input autoFocus ref={this.username} id="username" type="text" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
