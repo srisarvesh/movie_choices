9ef0cb19a3d56d15a5c3d720b16086c3125ace76
Sarvesh <r.srisarvesh@gmail.com>
2020-11-22 10:37:39 +0530

    Displaying validation errors

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-const Input = ({name,label,value,onChange}) => {
+const Input = ({name,label,value,onChange,error}) => {
     return ( 
         <div className="form-group">
                     <label htmlFor={name}>{label}</label>
@@ -10,7 +10,9 @@ const Input = ({name,label,value,onChange}) => {
                      type="text" 
                      className="form-control"
                      />
-                </div> 
+                
+                {error && <div className="alert alert-danger">{error}</div>}
+                </div>  
      );
 }
  
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,16 +2,38 @@ import ReactDOM from 'react-dom'
 import React, { Component } from 'react'; 
 import { Button } from 'bootstrap';
 import Input from './common/input'
+import { object } from 'prop-types';
 
 
 class LoginForm extends Component {
     state={
-        account:{username:"",password:""}
+        account:{username:"",password:""},
+        errors:{}
     };
-    username=React.createRef();
+   
+    validate=()=>{
+            const errors={};
+            const {account}=this.state;
+            if(account.username==='')
+            {
+                errors.username="Username is required";
+            }
+            if(account.password==='')
+            {
+                errors.password="Password is required";
+            }
+            return Object.keys(errors).length===0?null:errors;
+        }
+       
       handleSubmit=e=>{
             e.preventDefault();
-           
+            const errors=this.validate();
+            this.setState({errors:errors||{}})
+            if(errors)
+            {
+                return;
+            }
+           console.log("submitted")
         };
         handleChange=({currentTarget:input})=>{
             const account={...this.state.account};
@@ -19,16 +41,18 @@ class LoginForm extends Component {
             this.setState({account});
 
         }
+        
+        
     
     render() { 
-      const{account}=this.state;
+      const{account,errors}=this.state;
         return (
             
             <div className="container">
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-               <Input name={"username"} value={account.username} label="Username" onChange={this.handleChange}/>
-                <Input name={"password"} value={account.password} label="Password" onChange={this.handleChange}/>
+               <Input name={"username"} value={account.username} error={errors.username} label="Username" onChange={this.handleChange}/>
+                <Input name={"password"} value={account.password} error={errors.password} label="Password" onChange={this.handleChange}/>
                 
                 <button className="btn btn-primary">Login</button>
             </form>
