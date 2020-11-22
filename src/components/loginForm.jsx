51d0a9aaced71ab1809560111e3f9bcca34fd7e1
Sarvesh <r.srisarvesh@gmail.com>
2020-11-22 14:05:38 +0530

    Extracting the reusable form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,76 +4,30 @@ import { Button } from 'bootstrap';
 import Input from './common/input'
 import { object } from 'prop-types';
 import Joi from 'joi-browser'
+import Form from './common/form'
 
 
-class LoginForm extends Component {
+class LoginForm extends Form {
     state={
-        account:{username:"",password:""},
+        data:{username:"",password:""},
         errors:{}
     };
     schema={
         username:Joi.string().required().label("Username"),
         password:Joi.string().required().label("Password")
     }; 
-   
-    validate=()=>{
-            const options={abortEarly:false}
-            const {error}=Joi.validate(this.state.account,this.schema,options);
-            console.log(error);
-            if(!error) return null;
-
-            const errors={}
-            for(let item of error.details)
-            {
-                errors[item.path[0]]=item.message;
-            }
-            return errors;  
-        }
-        validateProprty=({name,value})=>{
-           const obj={[name]:value};
-           const schema={[name]:this.schema[name]};
-           const {error}=Joi.validate(obj,schema);
-           return error?error.details[0].message:null;
-
-        }
-       
-      handleSubmit=e=>{
-            e.preventDefault();
-            const errors=this.validate();
-            this.setState({errors:errors||{}})
-            if(errors)
-            {
-                return;
-            }
-           console.log("submitted")
-        };
-
-        handleChange=({currentTarget:input})=>{
-            const errors={...this.state.errors}
-            const errorMessage=this.validateProprty(input);
-            if(errorMessage)
-            {
-                errors[input.name]=errorMessage;
-            } 
-            else{
-                delete errors[input.name];
-            }
-            const account={...this.state.account};
-            account[input.name ]=input.value;
-            this.setState({account,errors});
-        }
-        
-        
-    
+       doSubmit=()=>{
+            console.log("submitted")
+        } 
     render() { 
-      const{account,errors}=this.state;
+      const{data,errors}=this.state;
         return (
             
             <div className="container">
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-               <Input name={"username"} value={account.username} error={errors.username} label="Username" onChange={this.handleChange}/>
-                <Input name={"password"} value={account.password} error={errors.password} label="Password" onChange={this.handleChange}/>
+               <Input name={"username"} value={data.username} error={errors.username} label="Username" onChange={this.handleChange}/>
+                <Input name={"password"} value={data.password} error={errors.password} label="Password" onChange={this.handleChange}/>
                 
                 <button 
                 disabled={this.validate()} 
