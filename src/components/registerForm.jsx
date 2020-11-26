11103e7aa18ebb15529593cbe03075b739c3be1c
Sarvesh <r.srisarvesh@gmail.com>
2020-11-26 12:55:37 +0530

    Redirecting the user after registering

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -27,7 +27,9 @@ class RegisterForm extends Form {
   doSubmit = async () => {
     // Call the server
     try{
-      await userService.register(this.state.data);
+     const response= await userService.register(this.state.data);
+     localStorage.setItem("token",response.headers["x-auth-token"]);
+     this.props.history.push("/");
     }
     catch(ex){
       if(ex.response&&ex.response.state===400)
