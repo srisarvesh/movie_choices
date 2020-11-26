b91783b95322d26201522082ef0523f1007c06a4
Sarvesh <r.srisarvesh@gmail.com>
2020-11-26 23:45:34 +0530

    After Hiding the delete column for the people who are not logged in and make the column visible for the person who logged in

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import auth from "../services/authService"
 import { Link } from "react-router-dom";
 import Table from "./common/table";
 import Like from "./common/like";
+
 
 class MoviesTable extends Component {
   columns = [
@@ -19,19 +21,25 @@ class MoviesTable extends Component {
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
     },
-    {
+    
+  ];
+  deleteColumn={
       key: "delete",
       content: movie => (
         <button
           onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
+          className="btn btn-danger btn-sm">
           Delete
         </button>
       )
-    }
-  ];
-
+    };
+constructor(){
+  super();
+  const user=auth.getCurrentUser();
+  if(user&&user.isAdmin){
+    this.columns.push( this.deleteColumn );
+  }
+}
   render() {
     const { movies, onSort, sortColumn } = this.props;
 
